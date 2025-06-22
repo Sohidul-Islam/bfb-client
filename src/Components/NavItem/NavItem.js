1d34f322f934f897d1f0bf64f84f62b9de297bc0
Sohidul-Islam <sishufol.sim@gmail.com>
2025-06-22 22:37:21 +0600

    test

--- src/Components/NavItem/NavItem.js
+++ src/Components/NavItem/NavItem.js
@@ -21,19 +21,34 @@ const NavItem = ({ navItem }) => {
             {
                 navItem.map((item, index) =>
                     <div key={index}>
-                        < div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block">
-                            < Link className='font-bold duration-200  hover:text-[#334154]' to={item.path}>{item.name}</Link>
+                        <div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154] text-white hover:text-[#334154] inline-block">
+                            {item.name.toLowerCase() === "home" ? (
+                                <Link
+                                    className="font-bold duration-200 hover:text-[#334154]"
+                                    to={item.path}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {item.name}
+                                </Link>
+                            ) : (
+                                <Link
+                                    className="font-bold duration-200 hover:text-[#334154]"
+                                    to={item.path}
+                                >
+                                    {item.name}
+                                </Link>
+                            )}
                         </div>
                     </div>
-
                 )
             }
 
-            {!user?.email && < div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block" >< Link className='font-bold duration-200  hover:text-[#334154]' to="/login">Login</Link></div>}
-            {user?.email && admin === true && < div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block" >< Link className='font-bold duration-200  hover:text-[#334154]' to="/admin">Admin</Link></div>}
+            {!user?.email && <div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block"><Link className='font-bold duration-200  hover:text-[#334154]' to="/login">Login</Link></div>}
+            {user?.email && admin === true && <div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block"><Link className='font-bold duration-200  hover:text-[#334154]' to="/admin">Admin</Link></div>}
 
-            {user?.email && <><span className='text-white'>{user?.displayName + " "}</span> <div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block" >< button className='font-bold duration-200  hover:text-[#334154]' onClick={signOutHandler}>Logout</button></div></>}
-        </div >
+            {user?.email && <><span className='text-white'>{user?.displayName + " "}</span> <div className="px-2 mx-2 py-3 hover:rounded duration-1000 hover:bg-[#EEE6E2] hover:text-[#334154]' text-white hover:text-[#334154] inline-block"><button className='font-bold duration-200  hover:text-[#334154]' onClick={signOutHandler}>Logout</button></div></>}
+        </div>
     )
 }
 
